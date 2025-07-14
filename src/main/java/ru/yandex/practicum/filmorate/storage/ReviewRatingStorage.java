4fe7c02ef343e3d61256643e1a1ae54f58926160
VladimirYazynin <vladimir.yazynin@inbox.ru>
2025-07-14 19:01:30 +0300

    Исправить code-style

--- src/main/java/ru/yandex/practicum/filmorate/storage/ReviewRatingStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/ReviewRatingStorage.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.storage;
 public interface ReviewRatingStorage {
 
     void addRatingOnReview(Long reviewId, Long userId, Boolean isLike);
+
     void removeRatingOnReview(Long reviewId, Long userId);
 
 }
