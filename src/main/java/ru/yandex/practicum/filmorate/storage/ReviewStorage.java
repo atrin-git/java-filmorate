f8e4e9eb5ee36855c68487c8fd14160d2d3fd298
VladimirYazynin <vladimir.yazynin@inbox.ru>
2025-07-14 19:08:04 +0300

    Исправить code-style

--- src/main/java/ru/yandex/practicum/filmorate/storage/ReviewStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/ReviewStorage.java
@@ -8,9 +8,13 @@ import java.util.Optional;
 public interface ReviewStorage {
 
     Optional<Review> find(Long reviewId);
+
     Review create(Review review);
+
     Review update(Review review);
+
     void delete(Long reviewId);
+
     Collection<Review> findLimitedReviews(Long filmId, Long count);
 
 }
